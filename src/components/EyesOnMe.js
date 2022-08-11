69b16993f0a6c17b8e91c0163bf13621e50ad66f
Verity Collins <verity.carlos@gmail.com>
2022-08-11 16:43:29 -0500

    commit tree 55c5b616637a94ced52e33d1186d9c00b3654046

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    function handleFocus(){
+        console.log('Good!')
+    }
+
+    return (
+        <button onBlur={handleBlur} onFocus={handleFocus} >Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React from 'react'
+
+function Keypad () {
+
+    function password(){
+        console.log("Entering password...")
+    }
+
+    return (
+        <div>
+            <h3>Enter Password Here</h3>
+            <input type="password" onChange={password}></input>
+        </div>
+    )
+}
+
+export default Keypad 
